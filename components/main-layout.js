0312c7f035448a16f5bb83ba64713ac56ba4a820
Dr-Positive <56218613+Dr-Positive@users.noreply.github.com>
2022-10-17 04:08:46 +0500

    test

--- components/main-layout.js
+++ components/main-layout.js
@@ -1,35 +1,24 @@
 import styles from "../components/main-layout.module.scss";
-import { ImgBox } from './img-box.js'
+import { ImgBox } from "./img-box.js";
 
-
-export function MainLayout({ }) {
+export function MainLayout({}) {
   return (
-      <div>        
-        <div className={styles.header}>
-        <ImgBox  src={'/image/svg/question-mark.svg'}  text={'50/60'}></ImgBox>
-         <ImgBox  src={'/image/svg/clock.svg'}  text={'60:60'} ></ImgBox>
-         <p className={styles.header__p}>
-          РУСЬ И ЗОЛОТАЯ ОРДА 
-         </p>
-         <ImgBox  src={'/image/png/mini-logo.png'} ></ImgBox>            
-        </div>
-          <div className={styles.container}>
-            <div className={styles.leftMenu}>
-                левая штука
-
-            </div >
-
-            <div className={styles.main}>
-                основа
-
-            </div >
-            
-
-            
+    <div>
+      <div className={styles.container}>
+        <div className={styles.leftMenu}>левая штука</div>
+        <div>
+          <div className={styles.header}>
+            <ImgBox
+              src={"/image/svg/question-mark.svg"}
+              text={"50/60"}
+            ></ImgBox>
+            <ImgBox src={"/image/svg/clock.svg"} text={"60:60"}></ImgBox>
+            <p className={styles.header__p}>РУСЬ И ЗОЛОТАЯ ОРДА</p>
+            <ImgBox src={"/image/png/mini-logo.png"}></ImgBox>
           </div>
+          <div className={styles.main}>это база</div>
+        </div>
       </div>
-
+    </div>
   );
 }
-
-
